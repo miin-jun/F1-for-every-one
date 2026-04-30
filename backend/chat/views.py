981bc9ca3b523bb5d41957b813cf386a25eff683
lifeisgoodlg <guswls3804@naver.com>
2026-04-30 10:02:00 +0900

    Feat:채팅 기록 저장 기능 구현 중

--- backend/chat/views.py
+++ backend/chat/views.py
@@ -3,4 +3,4 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def chat_index(request):
-    return render(request, 'chat/index.html')
+    return render(request, 'chat/chat_main.html')
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -6,6 +6,7 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path("chat/", include("chat.urls")),
     path("", include("front.urls")),
 ]
 
